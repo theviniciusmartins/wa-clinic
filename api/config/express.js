b9ab4e018eb339f6f233185bd552533828738162
Vinicius Martins <vinicius.martins@roundpe.gs>
2021-09-06 19:09:20 -0300

    fix deploy problem

--- api/config/express.js
+++ api/config/express.js
@@ -1,15 +1,11 @@
 const express    = require('express');
 const config     = require('config');
-const swagger    = require('swagger-ui-express');
-const swaggerDocument  = require('./swagger-document');
 
 module.exports = () => {
     const app = express();
 
     // SETANDO VARIÁVEIS DA APLICAÇÃO
     app.set('port', process.env.PORT || config.get('server.port'));
-    app.use(express.json())
-    app.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 
     return app;
 };
